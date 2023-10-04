a5753b5aebde48ed2ebf4d2260c084fa31e5b06d
Olya Vorontsova <16olya1996@gmail.com>
2023-10-04 19:28:47 +0200

    add Listener to btns

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,7 +2,31 @@ const bodyEl = document.querySelector('body');
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
 
+startBtn.addEventListener('click', onStart);
 
+let onInterval = false;
+stopBtn.disabled = true;
+
+function onStart() {
+    const timerId = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor();
+        onInterval = true;
+        console.log('I am ON');
+
+        if (onInterval) {
+            startBtn.disabled = true;
+            stopBtn.disabled = false;
+        }
+
+        stopBtn.addEventListener('click', onStop);
+        function onStop() {
+            clearInterval(timerId);
+            onInterval = false;
+            startBtn.disabled = false;
+            stopBtn.disabled = true;
+        }
+    }, 1000);
+}
 
 
 // random color function
