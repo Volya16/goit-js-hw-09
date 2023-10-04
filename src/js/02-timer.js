15ac2c4a95223e80418065210e350f13ec94a3e9
Olya Vorontsova <16olya1996@gmail.com>
2023-10-04 20:53:27 +0200

    got html elements

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,9 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+
+const inputEl = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const timerDays = document.querySelector('[data-days]');
+const timerHours = document.querySelector('[data-hours]');
+const timerMins = document.querySelector('[data-minutes]');
+const timerSec = document.querySelector('[data-seconds]');
