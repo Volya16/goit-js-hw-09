2c58ea4abc2e17bc0d8511232b566b77db8edd80
Olya Vorontsova <16olya1996@gmail.com>
2023-10-04 19:26:41 +0200

    get elements from html and add random color function

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,12 @@
+const bodyEl = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+
+
+
+// random color function
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
