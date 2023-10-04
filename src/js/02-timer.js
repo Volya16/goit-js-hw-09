12ead9828e8e35afe2a2bbd546f58ff211920a10
Olya Vorontsova <16olya1996@gmail.com>
2023-10-04 21:03:31 +0200

    make btn disabled

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,5 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+// import flatpickr from "flatpickr";
+// import "flatpickr/dist/flatpickr.min.css";
 
 const inputEl = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('[data-start]');
@@ -7,3 +7,6 @@ const timerDays = document.querySelector('[data-days]');
 const timerHours = document.querySelector('[data-hours]');
 const timerMins = document.querySelector('[data-minutes]');
 const timerSec = document.querySelector('[data-seconds]');
+
+startBtn.disabled = true;
+
