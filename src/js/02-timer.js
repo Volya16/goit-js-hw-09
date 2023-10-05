f8fae6606f3e5428cb172112e2f5fcabe5989014
Olya Vorontsova <16olya1996@gmail.com>
2023-10-05 23:22:15 +0200

    added styles

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,5 @@
-// import flatpickr from "flatpickr";
-// import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 
 const inputEl = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('[data-start]');
@@ -12,10 +12,19 @@ startBtn.disabled = true;
 
 const options = {
   enableTime: true,
+  // Включает выбор времени
   time_24hr: true,
+  //   24-часовой режим
   defaultDate: new Date(),
+  //   Устанавливает начальную выбранную дату
   minuteIncrement: 1,
+  //   Регулирует шаг ввода минут
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    // console.log(selectedDates[0]);
+    if (selectedDates[0] < Date.now()) {
+      alert('Please choose a date in the future');
+    } else {
+      startBtn.disabled = false;
+    }
   },
 };
