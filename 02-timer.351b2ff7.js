c9ead21d78029238b489bae040c9778598f3d07d
Volya16 <120327659+Volya16@users.noreply.github.com>
2023-10-05 21:22:48 +0000

    Deploying to gh-pages from @ Volya16/goit-js-hw-09@f8fae6606f3e5428cb172112e2f5fcabe5989014 🚀

--- 02-timer.351b2ff7.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#datetime-picker");const e=document.querySelector("[data-start]");document.querySelector("[data-days]"),document.querySelector("[data-hours]"),document.querySelector("[data-minutes]"),document.querySelector("[data-seconds]");e.disabled=!0;new Date;
-//# sourceMappingURL=02-timer.351b2ff7.js.map
